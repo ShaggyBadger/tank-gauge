967abaf82fcf595d468b742c1e7a9b7116805f4d
Josh <ShaggyBadger@users.noreply.github.com>
2025-04-27 22:12:15 -0400

    update

--- src/ui_maxInch.py
+++ src/ui_maxInch.py
@@ -8,6 +8,276 @@ from src import queries
 import settings
 import console
 import ui
+import math
+
+class fuelEntryScreen(ui.View):
+	def __init__(self, parent, data_packet):
+		super().__init__()
+		self.background_color = 'white'
+		self.appController = parent
+		self.subview_list = []
+		
+		# extract info from data_packet
+		self.data_packet = data_packet
+		self.store_num = data_packet['store_num']
+		self.tank_info = data_packet['tank_info']
+		self.switch_dict = data_packet['switch_dict']
+		
+		# build widgets
+		self.label1 = ui.Label()
+		self.label1.text = f'Store Number: {self.store_num}'
+		self.label1.size_to_fit()
+		self.label1.x = 20
+		self.label1.y = 20
+		
+		self.entry_dicts = self.build_entry_fields()
+		self.subview_list.append(self.label1)
+		
+		current_subview = self.label1
+		current_subview.y -= 30
+		
+		for subview in self.subview_list:
+			subview.y = current_subview.y + 30 + current_subview.height
+			current_subview = subview
+			self.add_subview(subview)
+	
+	def submit(self, sender):
+		self.data_packet['entry_dicts'] = self.entry_dicts
+		
+		self.appController.fuel_entry_results(self.data_packet)
+	
+	def build_entry_frame(self, data_set):
+		def slider_changed(sender):
+			linked_text_box = sender.linked_text_box
+			max_value = sender.max_value
+			slider_value = int(sender.value * max_value)
+			
+			linked_text_box.text = str(slider_value)
+		
+		def text_box_changed(sender):
+			text_box = sender.linked_text_box
+			slider = sender.linked_slider
+			gallons = int(text_box.text)
+			slider_position = gallons / slider.max_value
+			slider.value = slider_position
+			
+			
+			
+			
+		# unpack data
+		chart = data_set['chart']
+		inch_list = data_set['inch_list']
+		max_inch = data_set['max_inch']
+		max_gal = data_set['max_gal']
+		fuel_type = data_set['fuel_type']
+		tank = data_set['tank']
+		max_fill = math.floor(max_gal * 0.9)
+		
+		colors = {
+			'regular': '#000000',
+			'plus': '#4a87ff',
+			'premium': '#ff0000',
+			'kerosene': '#a55c37',
+			'diesel': '#ffcd6a'
+		}
+		
+		# build frame
+		frame = ui.View()
+		y = 0
+		
+		label1 = ui.Label()
+		label1.text = f'Fuel Type: '
+		
+		label1_1 = ui.Label()
+		label1_1.text_color = colors[fuel_type]
+		label1_1.text = f'{fuel_type}'
+		label1_1.font = ('<system-bold>', 16)
+		
+		label2 = ui.Label()
+		label2.text = f'Tank Name: '
+		label2_2 = ui.Label()
+		label2_2.text = f'{tank}'
+		label2_2.font = ('<system-bold>', 16)
+		
+		label3 = ui.Label()
+		label3.text = f'Tank Capacity: '
+		label3_3 = ui.Label()
+		label3_3.text = f'{max_gal}'
+		label3_3.font = ('<system-bold>', 16)
+		
+		label4 = ui.Label()
+		label4.text = f'90% Max Capacity: '
+		label4_4 = ui.Label()
+		label4_4.text = f'{max_fill}'
+		label4_4.font = ('<system-bold>', 16)
+		
+		text_box = ui.TextField()
+		slider = ui.Slider()
+		
+		text_box.placeholder = '0'
+		text_box.keyboard_type = ui.KEYBOARD_NUMBER_PAD
+		text_box.height = 30
+		text_box.border_color = settings.swto_blue
+		text_box.border_width = 1
+		text_box.corner_radius = 15
+		text_box.tint_color = settings.swto_blue
+		text_box.height = 50
+		text_box.linked_slider = slider
+		
+		slider.min_value = 0
+		slider.max_value = 8800
+		slider_continuous = True
+		slider.action = slider_changed
+		slider.tint_color = colors[fuel_type]
+		slider.corner_radius = 15
+		slider.height = 50
+		slider.linked_text_box = text_box
+		
+		btn = ui.Button()
+		btn.title = 'Enter'
+		btn.corner_radius = 15
+		btn.border_width = 1
+		btn.border_color = settings.swto_blue
+		btn.size_to_fit()
+		btn.width += 20
+		btn.height = 50
+		btn.linked_text_box = text_box
+		btn.linked_slider = slider
+		btn.action = text_box_changed
+		btn.background_color = settings.swto_blue
+		btn.tint_color = 'white'
+		
+		ui_elements = [
+			label1,
+			label1_1,
+			label2,
+			label2_2,
+			label3,
+			label3_3,
+			label4,
+			label4_4,
+			text_box,
+			slider,
+			btn
+			]
+		
+		for element in ui_elements:
+			element.size_to_fit()
+		
+		y = 0
+		
+		label1.x = 10
+		label1.y = y
+		y += label1.height + 10
+		
+		label1_1.x = label1.x + label1.width
+		label1_1.y = label1.y
+		
+		label2.x = 10
+		label2.y = y
+		y += label2.height + 10
+		
+		label2_2.x = label2.x + label2.width
+		label2_2.y = label2.y
+		
+		label3.x = 10
+		label3.y = y
+		y += label3.height + 10
+		
+		label3_3.x = label3.x + label3.width
+		label3_3.y = label3.y
+		
+		label4.x = 10
+		label4.y = y
+		y += label4.height + 10
+		
+		label4_4.x = label4.x + label4.width
+		label4_4.y = label4.y
+		
+		text_box.x = 10
+		text_box.y = y
+		y += text_box.height + 10
+		
+		btn.x = text_box.x + text_box.width + 10
+		btn.y = text_box.y
+		
+		slider.x = 10
+		slider.y = y
+		y += slider.height
+		
+		
+		
+		for element in ui_elements:
+			frame.add_subview(element)
+		
+		frame.height = y
+		frame.width = 320
+		
+		return frame
+		
+	
+	def build_entry_fields(self):
+		input_fields = []
+		
+		y = self.label1.y + 30
+		for switch in self.switch_dict:
+			if self.switch_dict[switch] is True:
+				tank_list = self.tank_info[switch]
+				for tank in tank_list:
+					chart = queries.get_tank_chart(tank)
+					inch_list = [
+						inch for inch in chart
+						]
+					max_inch = inch_list[-1]
+					max_gal = chart[max_inch]
+					
+					data_set = {
+						'chart': chart,
+						'inch_list': inch_list,
+						'max_inch': max_inch,
+						'max_gal': max_gal,
+						'fuel_type': switch,
+						'tank': tank
+					}
+					
+					entry_frame = self.build_entry_frame(data_set)
+					
+					info_box = ui.TextView()
+					info_box.text = f'Fuel Type: {switch}\n{tank}\nMax Gallons: {max_gal}\n90% Gallons: {math.floor(max_gal*.9)}'
+					info_box.width = 150
+					info_box.editable = False
+					info_box.x = self.label1.x
+					info_box.y = y
+					y = info_box.y + (info_box.height/2) + 20
+					
+					text_box = ui.TextField()
+					text_box.y = info_box.y + (info_box.height / 4)
+					text_box.x = info_box.x + (info_box.width)
+					text_box.height = 30
+					text_box.border_color = settings.swto_blue
+					text_box.border_width = 1
+					text_box.corner_radius = 5
+					text_box.placeholder ='0'
+					text_box.keyboard_type = ui.KEYBOARD_NUMBER_PAD
+					text_box.delegate = self
+					
+					entry_dict = {
+						'fuel_type': switch,
+						'tank_name': tank,
+						'text_box': text_box
+					}
+					input_fields.append(entry_dict)		
+				
+					#self.subview_list.append(info_box)
+					#self.subview_list.append(text_box)
+					self.subview_list.append(entry_frame)
+
+		return input_fields
+		
+	def touch_began(self, touch):
+		# this method allows the keyboard to go away when user touches screen
+		ui.end_editing()
+					
 
 class storeSelectScreen(ui.View):
 	def __init__(self, parent):
@@ -135,7 +405,6 @@ class storeSelectScreen(ui.View):
 		self.add_subview(self.dsl_lbl)
 		self.add_subview(self.dsl_switch)
 		
-	
 	def submit(self, sender):
 		switch_dict = {
 			'regular': self.reg_switch.value,
@@ -181,46 +450,41 @@ class storeSelectScreen(ui.View):
 		# this method allows the keyboard to go away when user touches screen
 		ui.end_editing()
  
-class fuelEntryScreen(ui.View):
-	def __init__(self, parent):
-		super().__init__()
-		self.background_color = 'blue'
-		
-		# build widgets
-		submit_btn = ui.Button(
-			title='Submit',
-			frame=(20,20,200,200),
-			action=self.submit
-			)
-		self.add_subview(submit_btn)
-	
-	def submit(self, sender):
-		self.appController.show_screen(reportScreen)
-
 class reportScreen(ui.View):
-	def __init__(self, parent):
+	def __init__(self, parent, data_packet):
 		super().__init__()
-		self.background_color = 'red'
+		self.background_color = 'white'
+		self.appController = parent
+		
+		# unpack data_packet
+		self.data_packet = data_packet
+		self.store_num = data_packet['store_num']
+		self.tank_info = data_packet['tank_info']
+		self.switch_dict = data_packet['switch_dict']
+		self.entry_dicts = data_packet['entry_dicts']
 		
 		# build widgets
-		submit_btn = ui.Button(
-			title='Submit',
+		reset_btn = ui.Button(
+			title='Reset',
 			frame=(20,20,200,200),
-			action=self.submit
+			action=self.reset,
+			border_color = settings.swto_blue,
+			border_width = 1,
+			corner_radius = 15
 			)
-		self.add_subview(submit_btn)
+		reset_btn.width += 20
+		self.add_subview(reset_btn)
 	
-	def submit(self, sender):
-		self.appController.show_screen(fuelEntryScreen)
+	def reset(self, sender):
+		new_view = storeSelectScreen(self.appController)
+		self.appController.show_screen(new_view)
 
 class appController(ui.View):
 	def __init__(self):
 		super().__init__()
 		self.current_screen = None
 		self.store_select_screen = storeSelectScreen(self)
-		self.fuel_entry_screen = fuelEntryScreen(self)
-		self.report_screen = reportScreen(self)
-
+		
 		self.show_screen(self.store_select_screen)
 	
 	def layout(self):
@@ -243,9 +507,26 @@ class appController(ui.View):
 		'''
 		take the arguments and fill out the fuelEntryScreen
 		'''
+		data_packet = {
+			'store_num': store_num,
+			'tank_info': tank_info,
+			'switch_dict': switch_dict
+		}
+		
+		view = fuelEntryScreen(self, data_packet)
 		
 		# end by bringing up appropriate view
-		self.show_screen(self.fuel_entry_screen)
+		self.current_screen = view
+		self.show_screen(view)
+	
+	def fuel_entry_results(self, data_packet):
+		view = reportScreen(self, data_packet)
+		self.current_screen = view
+		self.show_screen(view)
+	
+		
+		
+				
 
 if __name__ == '__main__':
 	from rich.traceback import install
